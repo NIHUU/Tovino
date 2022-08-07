989d4e9b382f1d3b76f370fe645c0bbd980ca5b2
NIHUU <106787129+NIHUU@users.noreply.github.com>
2022-08-07 11:20:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1030,7 +1030,7 @@ async def auto_filter(client, msg, spoll=False):
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"Pᴀɢᴇs", callback_data="pages"),
-             InlineKeyboardButton(text=f"{round(int(offset) / 10)", callback_data="pages"),
+             InlineKeyboardButton(text=f"1 - {round(int(total_results) / 10)}", callback_data="pages"),
              InlineKeyboardButton(text="Nᴇxᴛ", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
